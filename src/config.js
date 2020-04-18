7b04882b2a2aa27a9f2fba79be0740f3f2c01219
Sarah Loach <Sarah@Sarahs-fat-top.local>
2020-04-18 00:52:22 -0500

    updates for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASEB_URL: process.env.DATABASE_URL || 'postgresql://green-journal@localhost/green-journal',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://green-journal@localhost/green-journal',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://green-journal@localhost/green-journal-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
